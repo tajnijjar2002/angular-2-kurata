8e9700e750dec6d705240054ade78d703f872d60
tajveer nijjar <taj.nijjar2002@gmail.com>
2017-01-21 13:25:13 +1300

    Finished Retrieving data using HTTP

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -16,6 +16,7 @@ export class ProductListComponent implements OnInit{
     listFilter: string;
 
     products: IProduct[];
+    errorMessage: string;
 
     
     constructor(private _productService: ProductService)
@@ -30,7 +31,9 @@ export class ProductListComponent implements OnInit{
 
     ngOnInit():void
     {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+            error => this.errorMessage = <any>error);
         console.log("In OnInit");
     }
 
--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -21,9 +21,9 @@ export class ProductService
     getProducts(): Observable<IProduct[]>
     {
         return this._http.get(this._productUrl)
-        .map((response: Response) => <IProduct[]> response.json())
-        .do(data => console.log('All: ' + JSON.stringify(data)))
-        .catch(this.handleError);
+            .map((response: Response) => <IProduct[]> response.json())
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handleError);
     }
 
     private handleError(error: Response)
